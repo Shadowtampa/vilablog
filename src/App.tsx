3f5364e46647ac4e5386a576c16f88a9dcf2dc51
Luis Gomes <luisgomesbcc@gmail.com>
2025-10-14 20:36:03 -0300

    alterando configurações do blog

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,9 @@
-import { useEffect, useState } from "react";
-import { Header } from "./components/Header/Header"
-import { Landing } from "./pages/Landing/Landing"
-
-import axios from './service/axios';
-
+import Blog from "./blog/Blog"
 
 const App = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    axios.get('/posts', {
-      params: {
-        fetchImages: true,
-      }
-    })
-      .then(data => {
-        setData(data.data.items);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
-
   return (
     <>
-      <Header page="Blog" />
-      {data && <Landing articles={data} />}
-
+      <Blog />
     </>
   )
 }
