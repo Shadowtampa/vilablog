858b14bef74c0b59d8aff0ae8ab31272464dd02c
Luis Gomes <luisgomesbcc@gmail.com>
2025-10-18 17:36:42 -0300

    preparando pra trabalhar com paginação na instance axios

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-export const instance = axios.create({
+export const bloggerInstance = axios.create({
   baseURL: `https://www.googleapis.com/blogger/v3/blogs/${import.meta.env.VITE_BLOG_ID}/posts/`,
   timeout: 1000,
   params: {
     key: import.meta.env.VITE_BLOGGER_API_KEY,
+    maxResults: 10
   },
 });
